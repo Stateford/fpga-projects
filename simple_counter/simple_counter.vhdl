92dbc39cdc2d25ba74cb4d18ef26d0f4d65697a4
Dimitri Apostal <idietmoran@gmail.com>
2019-11-18 17:29:08 -0500

    Update simple_counter.vhdl

--- simple_counter/simple_counter.vhdl
+++ simple_counter/simple_counter.vhdl
@@ -4,7 +4,7 @@ USE ieee.std_logic_1164.all;
 ENTITY simple_counter IS
 	PORT(
 		clk			: IN std_logic;
-		count_up		: IN std_logic;
+		count_up	: IN std_logic;
 		count_down	: IN std_logic;
 		led			: OUT std_logic_vector(7 DOWNTO 0);
 		segment_out	: OUT std_logic_vector(6 DOWNTO 0)
@@ -15,8 +15,8 @@ ARCHITECTURE structure OF simple_counter IS
 
 	COMPONENT counter
 		PORT(
-			i_clk			: IN  std_logic;
-			i_up			: IN  std_logic;
+			i_clk		: IN  std_logic;
+			i_up		: IN  std_logic;
 			i_down		: IN  std_logic;
 			o_counter	: OUT std_logic_vector(7 DOWNTO 0)
 		);
@@ -25,27 +25,17 @@ ARCHITECTURE structure OF simple_counter IS
 	
 	COMPONENT segment
 		PORT(
-			i_bin	: IN  std_logic_vector(7 DOWNTO 0);
-			o_bcd	: OUT std_logic_vector(6 DOWNTO 0)
+			i_bin: IN  std_logic_vector(7 DOWNTO 0);
+			o_bcd: OUT std_logic_vector(6 DOWNTO 0)
 		);
 	END COMPONENT;
+
 	
-	COMPONENT debounce
-		PORT(
-			i_clk		: IN std_logic;
-			i_switch	: IN std_logic;
-			o_switch	: OUT std_logic
-		);
-	END COMPONENT;
-	
-	SIGNAL count	: std_logic_vector(7 DOWNTO 0);
-	SIGNAL d_count_up : std_logic;
-	SIGNAL d_count_down : std_logic;
+	SIGNAL count: std_logic_vector(7 DOWNTO 0);
 	
 BEGIN
-	D1: debounce PORT MAP(clk, count_up, d_count_up);
-	D2: debounce PORT MAP(clk, count_down, d_count_down);
-	G1: counter PORT MAP(clk, d_count_up, d_count_down, count);
+
+	G1: counter PORT MAP(clk, count_up, count_down, count);
 	S2: segment PORT MAP(count, segment_out);
 
 	led <= count;
